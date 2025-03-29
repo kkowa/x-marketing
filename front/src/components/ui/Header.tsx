ea059e0ab0986aa433c1ff62eaf82899a56c9d8f
Katarzyna Kowalska <k.a.kowalska@outlook.com>
2025-03-29 21:11:08 +0800

    feat: added a header component

--- front/src/components/ui/Header.tsx
+++ front/src/components/ui/Header.tsx
@@ -1,29 +1,53 @@
-import React from "react";
-import styles from ""; // Adjust the path as needed
+import React, { ReactNode } from "react";
+import { Button } from "./buttons/StandardButton";
+
+const styles = {
+  headerWrapper:
+    "relative flex text-black  flex-col md:flex-col sm:flex-col 2xl:flex-row xl:flex-row lg:flex-row 2xl:justify-between xl:justify-between lg:justify-between md:justify-center sm:justify-center justify-center 2xl:items-end xl:items-end lg:items-end md:items-center sm:items-center items-center w-full 2xl:-mt-95 xl:-mt-95 lg:-mt-75 md:-mt-[200px] sm:-mt-[160px] xs:-mt-[130px] -mt-[112px]",
+  textContainer:
+    "w-full 2xl:text-left xl:text-left lg:text-left md:text-center sm:text-center xs:text-center text-center md:pb-[min(12%,70px)] sm:pb-[min(12%,50px)] xs:pb-[min(12%,30px)] pb-[min(12%,50px)] sm:pb-[8%] 2xl:pb-0 xl:pb-0 lg:pb-0",
+  title:
+    "relative 2xl:text-6xl xl:text-6xl lg:text-5xl md:text-5xl sm:text-4xl xs:text-xl leading-[1.2] pb-0 whitespace-nowrap",
+  description:
+    "relative 2xl:text-base xl:text-base lg:text-base md:text-base sm:text-sm text-xs text-white leading-[1.5] mt-4 z-31 whitespace-nowrap overflow-hidden text-ellipsis",
+};
 
 interface HeaderProps {
   title: string;
-  description: string;
-  buttonElement?: React.FC<HeaderProps>;
+  description: string | ReactNode;
+  buttonContainer?: boolean;
+  buttonText?: string;
+  buttonIcon?: ReactNode;
 }
 
-export const Header: React.FC<HeaderProps> = ({
+const Header: React.FC<HeaderProps> = ({
   title,
   description,
-  buttonElement,
+  buttonContainer,
+  buttonText,
+  buttonIcon,
 }) => {
   return (
-    <div id="service-card-header" className={styles.headerWrapper}>
+    <div className={styles.headerWrapper}>
       <div className={styles.textContainer}>
-        <div className={`title-color ${styles.title}`}>
-          {title}
-          <br className="hidden 2xl:block xl:block lg:block" /> with us
-        </div>
+        <div className={`title-color ${styles.title}`}>{title}</div>
         <div className={styles.description}>{description}</div>
       </div>
-      <div className={styles.buttonContainer}>
-        {buttonElement && React.createElement(buttonElement)}
-      </div>
+      {buttonContainer && (
+        <div>
+          <Button
+            variant="outline"
+            size="csm"
+            radius="full"
+            text={buttonText}
+            className="2xl:w-full xl:w-full lg:w-full md:w-full sm:w-full w-full 2xl:h-12 xl:h-12 lg:h-12 md:h-12 sm:h-12 h-10 2xl:px-6 xl:px-6 lg:px-6 md:px-6 sm:px-6 px-4"
+          >
+            {buttonIcon}
+          </Button>
+        </div>
+      )}
     </div>
   );
 };
+
+export default Header;
