a6fc2b8e8a52dc875d7191783bf6139391a59a9a
Katarzyna Kowalska <k.a.kowalska@outlook.com>
2025-03-30 19:46:17 +0800

    feat: portfolio card grid

--- front/src/components/ui/cards/PortfolioCard.tsx
+++ front/src/components/ui/cards/PortfolioCard.tsx
@@ -33,30 +33,32 @@ const PortfolioCard: React.FC<PortfolioCardProps> = ({
         className="mb-6"
       >
         <div className="flex relative">
-          <div className="flex flex-col justify-between flex-grow w-1/2 px-8 py-10">
+          <div className="flex flex-col justify-between flex-grow w-1/2 pl-[24px] py-[34px]">
             <div className="relative">
-              <div className="relative w-42 mb-6">
+              <div className="relative w-34 ">
                 <Image src={LinearBorder} alt="" className="w-full" />
-                <CardDescription className="absolute inset-0 z-10 flex items-center justify-center text-[#0A0C14]">
+                <CardDescription className="absolute inset-0 z-10 flex items-center justify-center text-[#0A0C14] text-[12px]">
                   {cardDescription}
                 </CardDescription>
               </div>
-              <CardTitle className="text-4xl text-[#0A0C14]">
+              <CardTitle className="text-[32px] text-[#0A0C14]">
                 {cardTitle}
               </CardTitle>
             </div>
-            <CardContent className="text-[#494C58]">{cardContent}</CardContent>
+            <CardContent className="text-[#494C58] text-[14px]">
+              {cardContent}
+            </CardContent>
           </div>
 
-          <div className="w-1/2 relative overflow-hidden">
-            <div className="relative top-5 left-0 -right-10 bottom-0">
-              <div className="absolute -top-2 right-5 z-20">
+          <div className="w-[55%] relative overflow-hidden flex flex-col justify-end">
+            <div className="relative">
+              <div className="absolute -top-5 right-0 z-20">
                 <Image src={Icon} alt="" />
               </div>
               <Image
                 src={imageSRC}
                 alt={imageAlt}
-                className="w-full h-full object-contain"
+                className="w-full object-contain"
               />
             </div>
           </div>
--- front/src/components/website/home/Portfolio.tsx
+++ front/src/components/website/home/Portfolio.tsx
@@ -3,9 +3,9 @@ import Header from "@/components/ui/Header";
 import { ArrowUpCircleIcon } from "public/icons/Icons";
 
 import Image1 from "public/img/png/portfolio/img-1.png";
-// import Image2 from "public/img/png/portfolio/img-2.png";
-// import Image3 from "public/img/png/portfolio/img-3.png";
-// import Image4 from "public/img/png/portfolio/img-4.png";
+import Image2 from "public/img/png/portfolio/img-2.png";
+import Image3 from "public/img/png/portfolio/img-3.png";
+import Image4 from "public/img/png/portfolio/img-4.png";
 
 const Portfolio = () => {
   return (
@@ -23,20 +23,66 @@ const Portfolio = () => {
         buttonText="All Portfollio"
         buttonIcon={<ArrowUpCircleIcon color="black" />}
       />
-      <PortfolioCard
-        cardDescription="Development Project"
-        cardTitle="MStore"
-        cardContent={
-          <>
-            Mangcoding is a biggest company in <br /> Indonesia, who provides
-            the
-          </>
-        }
-        imageSRC={Image1}
-        imageAlt=""
-        bottomTitle="MStore"
-        bottomDescription="Mangcoding is a biggest company in Indonesia, who provides the services in Development Website, Shopify and Wordpress"
-      />
+      <div className="grid grid-cols-2 gap-6">
+        <PortfolioCard
+          cardDescription="Development Project"
+          cardTitle="MStore"
+          cardContent={
+            <>
+              Mangcoding is a biggest company in <br /> Indonesia, who provides
+              the
+            </>
+          }
+          imageSRC={Image1}
+          imageAlt=""
+          bottomTitle="MStore"
+          bottomDescription="Mangcoding is a biggest company in Indonesia, who provides the services in Development Website, Shopify and Wordpress"
+        />
+        <PortfolioCard
+          cardDescription="Development Project"
+          cardTitle="Beauty"
+          cardContent={
+            <>
+              Mangcoding is a biggest company in <br /> Indonesia, who provides
+              the
+            </>
+          }
+          imageSRC={Image2}
+          imageAlt=""
+          bottomTitle="Beauty"
+          bottomDescription="Mangcoding is a biggest company in Indonesia, who provides the services in Development Website, Shopify and Wordpress"
+        />
+      </div>
+      <div className="grid grid-cols-2 gap-6">
+        <PortfolioCard
+          cardDescription="Development Project"
+          cardTitle="Jual"
+          cardContent={
+            <>
+              Mangcoding is a biggest company in <br /> Indonesia, who provides
+              the
+            </>
+          }
+          imageSRC={Image3}
+          imageAlt=""
+          bottomTitle="Mangcoding"
+          bottomDescription="Mangcoding is a biggest company in Indonesia, who provides the services in Development Website, Shopify and Wordpress"
+        />
+        <PortfolioCard
+          cardDescription="Development Project"
+          cardTitle="MangStore"
+          cardContent={
+            <>
+              Mangcoding is a biggest company in <br /> Indonesia, who provides
+              the
+            </>
+          }
+          imageSRC={Image4}
+          imageAlt=""
+          bottomTitle="MangStore"
+          bottomDescription="Mangcoding is a biggest company in Indonesia, who provides the services in Development Website, Shopify and Wordpress"
+        />
+      </div>
     </div>
   );
 };
