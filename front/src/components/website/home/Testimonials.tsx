fc22fe956393e55c0cea6bfc4b6fbce94c59acef
Katarzyna Kowalska <k.a.kowalska@outlook.com>
2025-03-31 15:41:00 +0800

    feat: testimonials header

--- front/src/components/website/home/Testimonials.tsx
+++ front/src/components/website/home/Testimonials.tsx
@@ -1,5 +1,21 @@
+import Header from "@/components/ui/Header";
+
 const Testimonials = () => {
-  return <div>Testimonials</div>;
+  return (
+    <div className="mt-12">
+      <Header
+        title="Our Product"
+        description={
+          <>
+            help you to build website company that is modern, user friendly
+            <br />
+            good CEO, and Clean design
+          </>
+        }
+        buttonContainer={false}
+      />
+    </div>
+  );
 };
 
 export default Testimonials;
