1dba49ee0782fb8732752611a025849510aafaa5
Katarzyna Kowalska <k.a.kowalska@outlook.com>
2025-04-02 22:17:27 +0800

    fix: removed unnessesery style class

--- front/src/app/(website)/about/page.tsx
+++ front/src/app/(website)/about/page.tsx
@@ -1,9 +1,7 @@
-import styles from "@/styles/about/About.module.scss";
-
 export default function About() {
   return (
     <>
-      <div className={styles.container}>
+      <div>
         <h1>About Us</h1>
         <p>We are a company dedicated to providing the best services.</p>
       </div>
